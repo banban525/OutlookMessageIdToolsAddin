a1abed907a1f2266824b5adde065a6c6695c1d61
banban525 <takakata@banban55.sakura.ne.jp>
2015-06-05 21:06:41 +0900

    add chocolatey's package source.

---
+++ Source/Chocolatey/OutlookMessageIdToolsAddin/tools/chocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+﻿
+$key = get-ItemProperty -path registry::HKEY_CURRENT_USER\Software\banban525\MessageIDTools
+
+$productCode = $key.ProductCode
+
+Start-Process "msiexec" "/quiet /uninstall $productCode" -Wait
